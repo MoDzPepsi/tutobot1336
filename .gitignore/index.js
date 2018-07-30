f3587a24d88b928f8e082b0808fd7260e193b664
MoDzPepsi <41086050+MoDzPepsi@users.noreply.github.com>
2018-07-30 20:11:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -444,38 +444,3 @@ if(message.content.startsWith(prefix +'ytsearch')){
 
                                                 bot.login('NDQ5MTIwOTU3NTg0Mzc1ODA4.DetpZg.8q3raoHbnMdAbAy5aQd75vNR1tA');
 
-const Roles = require("./roles_manager.js");
-const DiscordBot = require("./bot.js");
-const fs = require("fs");
-
-function log(msg) {
-	var date = new Date();
-	var dateStr = date;
-	fs.appendFile("log", dateStr+ " "+msg+"\n", function() {
-		consoleOutput(dateStr +" "+ msg);
-	});
-}
-
-consoleOutput = console.log;
-console.log = function() {
-	msg = "";
-	for(var i = 0 ; i < arguments.length ; i++) {
-		msg += arguments[i] + " ";
-	}
-	log(msg)
-}
-
-function onBotReady(){
-	Roles.setDiscordBot(DiscordBot);
-	updateRoles();
-	setInterval(updateRoles, 1000*60*60);
-	setInterval(processQueue, 500);
-}
-
-function updateRoles(){
-	Roles.updateEveryGuildsRoles(DiscordBot);
-}
-
-function processQueue(){
-	Roles.processQueue();
-}
